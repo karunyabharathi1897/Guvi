376892824c6373f0bc316a785725c301f9195d3d
karunyabharathi1897 <karunyabharathi1897@gmail.com>
2017-12-19 14:29:38 +0530

    Create Alphabet or not.c

---
+++ C Programming/Codekata/Beginner/Set1/Alphabet or not.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+char c;
+    //printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf(" alphabet.");
+    else
+        printf("not an alphabet.");
+return 0;
+}
