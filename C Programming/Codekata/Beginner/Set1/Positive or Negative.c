dd0384161c29f0866acc583e4318e27218031b9b
karunyabharathi1897 <karunyabharathi1897@gmail.com>
2017-12-19 12:16:10 +0530

    Update Positive or Negative.c

--- C Programming/Codekata/Beginner/Set1/Positive or Negative.c
+++ C Programming/Codekata/Beginner/Set1/Positive or Negative.c
@@ -1,1 +1,22 @@
-
+    #include <stdio.h>
+     
+    int main(void) {
+    	// your code goes here
+    	int a;
+    	scanf("%d",&a);
+    	if(a==0)
+    	{
+    		printf("given number is zero");
+     
+    	}
+    	else if(a>0)
+    	{
+    		printf("given number is positive");
+    	}
+    	else
+    	{
+    		printf("given number is negative");
+    	}
+    	return 0;
+    }
+     
